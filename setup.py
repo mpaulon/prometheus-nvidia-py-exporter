205e49c381378ef523af47adad058ff46706e06c
Michaël Paulon <michael@paulon.org>
2021-09-13 14:26:32 +0200

    add systemd example file

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
 setup(
-    scripts=["scripts/prometheus-nvidia-py-exporter"]
+    scripts=["scripts/prometheus-nvidia-py-exporter"],
+    include_package_data=True
 )
