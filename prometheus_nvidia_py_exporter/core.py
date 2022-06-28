6109ecc89143fc9b0583c57fe00a7b0346b49bfa
Michaël Paulon <michael@paulon.org>
2022-06-28 16:29:26 +0200

    clear at the right place

--- prometheus_nvidia_py_exporter/core.py
+++ prometheus_nvidia_py_exporter/core.py
@@ -43,6 +43,9 @@ class AppMetrics:
         """
         pynvml.nvmlInit()
         self.num_devices = pynvml.nvmlDeviceGetCount()
+        self.compute_process_memory.clear()
+        self.graphics_process_memory.clear()
+        self.MPScompute_process_memory.clear()
         for i in range(self.num_devices):
             handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             labels = [
@@ -59,15 +62,15 @@ class AppMetrics:
             self.temperature.labels(*labels).set(pynvml.nvmlDeviceGetTemperature(handle, pynvml.NVML_TEMPERATURE_GPU))
             self.fan_speed.labels(*labels).set(pynvml.nvmlDeviceGetFanSpeed(handle))
 
-            self.compute_process_memory.clear()
+            
             for p in pynvml.nvmlDeviceGetComputeRunningProcesses(handle):
                 p_labels = [p.pid, pynvml.nvmlSystemGetProcessName(p.pid).decode()]
                 self.compute_process_memory.labels(*labels, *p_labels).set(p.usedGpuMemory)
-            self.graphics_process_memory.clear()
+            
             for p in pynvml.nvmlDeviceGetGraphicsRunningProcesses(handle):
                 p_labels = [p.pid, pynvml.nvmlSystemGetProcessName(p.pid).decode()]
                 self.graphics_process_memory.labels(*labels, *p_labels).set(p.usedGpuMemory)
-            self.MPScompute_process_memory.clear()
+            
             for p in pynvml.nvmlDeviceGetMPSComputeRunningProcesses(handle):
                 p_labels = [p.pid, pynvml.nvmlSystemGetProcessName(p.pid).decode()]
                 self.MPScompute_process_memory.labels(*labels, *p_labels).set(p.usedGpuMemory)
